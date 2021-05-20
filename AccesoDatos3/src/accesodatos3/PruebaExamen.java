8a08ff71acd415afd89868e86e9ed71763fb7fce
aurba <aurba@192.168.14.225>
2021-05-20 21:28:47 +0200

    fichero nuevo PruebaExamen.java

---
+++ AccesoDatos3/src/accesodatos3/PruebaExamen.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accesodatos3;
+
+/**
+ *
+ * @author aurba
+ */
+public class PruebaExamen {
+    
+      public void pruebaTrimestral(){
+        System.out.println("Creacion de fichero de prueba");
+    }
+    
+    
+}
